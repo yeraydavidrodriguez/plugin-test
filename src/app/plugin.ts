dd1553009f47cc99eba2598c7f2ec364426aa8f3
Yeray David Rodríguez <yeray.rodriguez@edataconsulting.de>
2023-01-17 07:35:55 +0000

    Call to height

--- src/app/plugin.ts
+++ src/app/plugin.ts
@@ -9,9 +9,16 @@ export class DemoPlugin {
             debugelement.innerText = '---'+JSON.stringify(data)+'---';
         });
 
+        document.getElementById('boton')?.addEventListener('click', (e) => {
+            parent.postMessage({iss: "4de59b9c-211b-4b38-bea8-9b9bf38ad9de-3d53c666-6f61-4a61-bb4c-232ddd97bb76", 
+                               sub: 'setHeight', height: '120'}, "*")
+        })
+
         const debugelement = document.getElementById('info')!;
         debugelement.innerText = "Loading";
     }
+
+
 
     private changeName(userName: string) {
         const nameElem = document.getElementById('userName')!;
